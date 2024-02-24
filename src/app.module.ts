a6d84b8251946f208f5ad75e3c1b2bb98023b8d6
Brian Ding <brian_ding@outlook.com>
2024-02-24 16:18:42 +0800

    add PingController to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ResponseInterceptor } from './interceptors';
 import { OpenAIController } from './api/openai/controllers';
+import { PingController } from './api/ping';
 
 @Module({
   imports: [],
   controllers: [
     OpenAIController,
+    PingController,
   ],
   providers: [
     {
